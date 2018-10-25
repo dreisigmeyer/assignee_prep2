1a1f273e3f72cf526966e9a6d54bbb4aba7760d8
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-10-25 09:20:48 -0400

    modified get_iops.sh

--- get_iops.sh
+++ get_iops.sh
@@ -3,8 +3,8 @@
 rm -f trash.csv
 rm -f iops.csv
 
-grep -h "INDIVIDUALLY OWNED PATENT" ./outData/*.csv | awk -F'|' -v OFS='|' '{print $1,$5,$3,$6,$4,$10,$11}' | sort -t'|' -u >> trash.csv
-awk -F'|' -v OFS='|' '{ if ($6 == "04" || $6 =="05") {print $1,$5,$3,$6,$4,$10,$11}}' ./outData/*.csv | sort -t'|' -u >> trash.csv
+grep -h "INDIVIDUALLY OWNED PATENT" ./out_data/*.csv | awk -F'|' -v OFS='|' '{print $1,$5,$3,$6,$4,$10,$11}' | sort -t'|' -u >> trash.csv
+awk -F'|' -v OFS='|' '{ if ($6 == "04" || $6 =="05") {print $1,$5,$3,$6,$4,$10,$11}}' ./out_data/*.csv | sort -t'|' -u >> trash.csv
 sort -t'|' -u trash.csv > iops.csv
 
 rm trash.csv
